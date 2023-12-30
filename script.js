388fb8f1c480095b347a708ea89261937c8bcdd1
Vivek Kumar <vkjha2511@gmail.com>
2023-12-31 00:26:41 +0530

    Skills section completed

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  var typeData = new Typed(".role", {
+    strings: [
+      "Coder",
+      "Software Developer",
+      "Full Stack Developer",
+      "Web Developer",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 0,
+  });
+});
