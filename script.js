ad40893e3327b33030673e7f75c9d31ffe6ede92
Vivek Kumar <vkjha2511@gmail.com>
2023-12-31 12:08:48 +0530

    Completed

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ document.addEventListener("DOMContentLoaded", function () {
     backDelay: 0,
   });
 });
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_56f847q";
+  const templateID = "template_8z2cvzg";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Your message sent successfully!!");
+    })
+    .catch((err) => console.log(err));
+}
